45f48c05c43afa1c79df5526ee7968cf922caf9c
matiasvenditti <mativenditti@gmail.com>
2020-06-28 19:14:24 -0300

    keyspace creation on starup and added docker compose file

--- src/main/java/com/bigdata/bigdata/config/CassandraConfig.java
+++ src/main/java/com/bigdata/bigdata/config/CassandraConfig.java
@@ -1,14 +1,17 @@
 package com.bigdata.bigdata.config;
 
-import com.datastax.oss.driver.api.core.CqlSession;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.cassandra.config.AbstractCassandraConfiguration;
 import org.springframework.data.cassandra.config.SchemaAction;
+import org.springframework.data.cassandra.core.cql.keyspace.CreateKeyspaceSpecification;
+import org.springframework.data.cassandra.core.cql.keyspace.DropKeyspaceSpecification;
+import org.springframework.data.cassandra.core.cql.keyspace.KeyspaceOption;
 
-import java.net.InetSocketAddress;
+import java.util.Collections;
+import java.util.List;
 
+@Configuration
 public class CassandraConfig extends AbstractCassandraConfiguration {
 
     @Value("${spring.data.cassandra.contact-points:placeholder}")
@@ -20,11 +23,11 @@ public class CassandraConfig extends AbstractCassandraConfiguration {
     @Value("${spring.data.cassandra.keyspace-name:placeholder}")
     private String keySpace;
 
-    @Value("${spring.data.cassandra.schema-action}")
-    private String schemaAction;
-
     @Value("${spring.data.cassandra.local-datacenter}")
     private String localDataCenter;
+
+    @Value("${cassandra.basepackages}")
+    private String basePackages;
 
     @Override
     protected String getKeyspaceName() {
@@ -43,39 +46,32 @@ public class CassandraConfig extends AbstractCassandraConfiguration {
 
     @Override
     public SchemaAction getSchemaAction() {
-        return SchemaAction.valueOf(schemaAction);
+        return SchemaAction.CREATE_IF_NOT_EXISTS;
+    }
+
+    @Override
+    public String[] getEntityBasePackages() {
+        return new String[] {basePackages};
     }
 
     @Override
     protected String getLocalDataCenter() {
         return localDataCenter;
     }
-}
 
-//@Configuration
-//public class CassandraConfig {
-//
-//    @Value("${spring.data.cassandra.contact-points:placeholder}")
-//    private String contactPoints;
-//
-//    @Value("${spring.data.cassandra.port:0000}")
-//    private int port;
-//
-//    @Value("${spring.data.cassandra.keyspace-name:placeholder}")
-//    private String keySpace;
-//
-//    @Value("${spring.data.cassandra.schema-action}")
-//    private String schemaAction;
-//
-//    @Value("${spring.data.cassandra.local-datacenter}")
-//    private String localDataCenter;
-//
-//    public @Bean CqlSession session() {
-//        return CqlSession.builder()
-//                .withKeyspace(keySpace)
-//                .withLocalDatacenter("datacenter1")
-//                .addContactPoint(new InetSocketAddress(contactPoints, port))
-//                .build();
-//    }
-//
-//}
+    @Override
+    protected List<CreateKeyspaceSpecification> getKeyspaceCreations() {
+        final CreateKeyspaceSpecification specification =
+                CreateKeyspaceSpecification.createKeyspace(keySpace)
+                .ifNotExists()
+                .with(KeyspaceOption.DURABLE_WRITES, true)
+                .withSimpleReplication();
+
+        return Collections.singletonList(specification);
+    }
+
+    @Override
+    protected List<DropKeyspaceSpecification> getKeyspaceDrops() {
+        return Collections.singletonList(DropKeyspaceSpecification.dropKeyspace(keySpace));
+    }
+}
