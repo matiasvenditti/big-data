4fa7fe1510a8e8da3ed6522a668bfaaed4ae13d1
David Cerbino <david.cerbino@mulesoft.com>
2020-07-09 18:08:24 -0300

    added es Servcies

--- src/main/java/com/bigdata/bigdata/controller/EsController.java
+++ src/main/java/com/bigdata/bigdata/controller/EsController.java
@@ -3,22 +3,26 @@ package com.bigdata.bigdata.controller;
 import com.bigdata.bigdata.model.Log;
 import com.bigdata.bigdata.repository.EsRepository;
 import com.bigdata.bigdata.repository.LogRepository;
+import com.bigdata.bigdata.service.EsServices;
 import org.elasticsearch.action.search.SearchRequest;
-import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestHighLevelClient;
-import org.elasticsearch.search.SearchHit;
 import org.elasticsearch.search.SearchHits;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
-import java.util.Collections;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Optional;
 
@@ -27,33 +31,34 @@ public class EsController {
 
     private final EsRepository esRepository;
     private final LogRepository logRepository;
-    private final RestHighLevelClient client;
+    private final EsServices esServices;
 
     @Autowired
-    public EsController(EsRepository esRepository, LogRepository logRepository,RestHighLevelClient client) {
+    public EsController(EsRepository esRepository, LogRepository logRepository, EsServices esServices) {
         this.esRepository = esRepository;
         this.logRepository = logRepository;
-        this.client = client;
+        this.esServices = esServices;
     }
 
-    @GetMapping(path = "/es")
-    public ResponseEntity<String> queryElasticSearch() {
-        Optional<Log> optional = logRepository.findLatestRecord();
-        if (optional.isPresent()) {
-            Pageable pageable = PageRequest.of(0, 10);
-            List<Log> queryResults = esRepository.findLatestBatch(optional.get().getTimestamp(), pageable);
-            return new ResponseEntity<>(queryResults.toString(), HttpStatus.OK);
-        }
+    @GetMapping(path = "/es/count")
+    public ResponseEntity<Long> queryElasticSearch() {
+        final Optional<Long> result = esServices.countAll();
 
-        SearchRequest searchRequest = new SearchRequest();
-        searchRequest.indices("shorewall");
-
-        try {
-            final SearchHits hits = client.search(searchRequest, RequestOptions.DEFAULT).getHits();
-            return new ResponseEntity<String>(hits.getHits().toString(),HttpStatus.OK);
-        } catch (IOException e) {
-            e.printStackTrace();
-            return new ResponseEntity<>("", HttpStatus.NOT_FOUND);
-        }
+        if (result.isPresent()){
+            return new ResponseEntity<>(result.get(),HttpStatus.OK);
+        } else return new ResponseEntity<>(-1L, HttpStatus.NOT_FOUND);
     }
+    @GetMapping(path = "/es",produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Object> getAll(){
+        final Iterator<JSONObject> iterator = esServices.getAll(5);
+        JSONArray array = new JSONArray();
+        while (array.length()<10 && iterator.hasNext()){
+            array.put(iterator.next());
+        }
+
+        return ResponseEntity.ok().body(array.toString());
+//        return new ResponseEntity<>(array.toString(),HttpStatus.OK);
+    }
+
+
 }
---
+++ src/main/java/com/bigdata/bigdata/service/EsServices.java
@@ -0,0 +1,132 @@
+package com.bigdata.bigdata.service;
+
+import org.apache.lucene.search.TotalHits;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.action.search.SearchScrollRequest;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.client.core.CountRequest;
+import org.elasticsearch.client.core.CountResponse;
+import org.elasticsearch.common.unit.TimeValue;
+import org.elasticsearch.index.query.QueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+@Service
+public class EsServices {
+    public static final String SHOREWALL = "shorewall";
+    private final RestHighLevelClient client;
+
+    @Autowired
+    public EsServices(RestHighLevelClient client) {
+        this.client = client;
+    }
+
+//    public Optional<Long> countAll() {
+//        SearchRequest searchRequest = new SearchRequest(SHOREWALL);
+//        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+//
+//        searchSourceBuilder.query(QueryBuilders.matchAllQuery());
+//        searchRequest.source(searchSourceBuilder);
+//        searchRequest.scroll(TimeValue.timeValueMinutes(1L));
+//
+//        long count = 0;
+//
+//        try {
+//            final SearchResponse searchResponse = client.search(searchRequest, RequestOptions.DEFAULT);
+//            final String scrollId = searchResponse.getScrollId();
+//            final SearchScrollRequest scrollRequest = new SearchScrollRequest(scrollId);
+//            scrollRequest.scroll(TimeValue.timeValueSeconds(30));
+//
+//            return Optional.of(client.search(searchRequest, RequestOptions.DEFAULT).getHits().getTotalHits().value);
+//        } catch (IOException e) {
+//            return Optional.empty();
+//        }
+//    }
+
+    public Optional<Long> countAll() {
+        CountRequest countRequest = new CountRequest(SHOREWALL);
+        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+        searchSourceBuilder.query(QueryBuilders.matchAllQuery());
+
+        try {
+            return Optional.of(client.count(countRequest, RequestOptions.DEFAULT).getCount());
+
+        } catch (IOException e) {
+            return Optional.empty();
+        }
+    }
+
+    public Iterator<JSONObject> getAll(int pageSize){
+        SearchRequest searchRequest = new SearchRequest(SHOREWALL);
+        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+        searchSourceBuilder.query(QueryBuilders.matchAllQuery());
+        searchSourceBuilder.size(pageSize);
+        searchRequest.source(searchSourceBuilder);
+        searchRequest.scroll(TimeValue.timeValueMinutes(1L));
+        try {
+            SearchResponse searchResponse = client.search(searchRequest, RequestOptions.DEFAULT);
+            return new serachIterator(searchResponse,client);
+        } catch (IOException e) {
+            return Collections.emptyIterator();
+        }
+
+    }
+
+    public Iterator<JSONObject> getAll(){
+        return getAll(10);
+    }
+
+    private static class serachIterator implements Iterator<JSONObject>{
+
+        private final RestHighLevelClient client;
+        private final SearchResponse searchResponse;
+        private Iterator<SearchHit> iterator;
+        private String scrollId;
+
+
+        public serachIterator(SearchResponse searchResponse,RestHighLevelClient client) {
+            this.searchResponse = searchResponse;
+            iterator= searchResponse.getHits().iterator();
+            scrollId= searchResponse.getScrollId();
+            this.client = client;
+        }
+
+        @Override
+        public boolean hasNext() {
+            if (iterator.hasNext()) return true;
+            else {
+                SearchScrollRequest scrollRequest = new SearchScrollRequest(scrollId);
+                scrollRequest.scroll(TimeValue.timeValueSeconds(30));
+                try {
+                    SearchResponse searchScrollResponse = client.scroll(scrollRequest,RequestOptions.DEFAULT);
+                    searchScrollResponse = client.scroll(scrollRequest, RequestOptions.DEFAULT);
+
+                    scrollId = searchScrollResponse.getScrollId();
+                    iterator = searchScrollResponse.getHits().iterator();
+                } catch (IOException e) {
+                    iterator = Collections.emptyIterator();
+                }
+                return iterator.hasNext();
+            }
+        }
+
+        @Override
+        public JSONObject next() {
+            if (hasNext()) return new JSONObject(iterator.next());
+            else throw new NoSuchElementException();
+        }
+    }
+}
